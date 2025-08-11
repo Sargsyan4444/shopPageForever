2e702b69975737eece08aa11ce3bcbf7a258d495
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-08-11 07:52:25 -0700

    i added full navbar part with responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets.js'
 import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const [visible, setVisible] = useState(false);
   return (
     <div className='flex items-center justify-between py-5 font-medium '>
         
@@ -42,7 +44,23 @@ const Navbar = () => {
          <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
          <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
         </Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
     </div>
+
+    {/* Slidebar Menu for small screen*/}
+    <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? "w-full" : "w-0"}`}>
+    <div className='flex flex-col text-gray-600'>
+      <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+        <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+        <p>Back</p>
+      </div>
+      <NavLink onClick={()=>setVisible(false)}  to="/"  className="py-2 pl-6 border">HOME </NavLink>
+      <NavLink onClick={()=>setVisible(false)}  to="/collection"  className="py-2 pl-6 border">COLLECTION </NavLink>
+      <NavLink onClick={()=>setVisible(false)}  to="/about"  className="py-2 pl-6 border">ABOUT </NavLink>
+      <NavLink onClick={()=>setVisible(false)}  to="/contact"  className="py-2 pl-6 border">CONTACT </NavLink>
+    </div>
+    </div>
+
     </div>
   )
 }
