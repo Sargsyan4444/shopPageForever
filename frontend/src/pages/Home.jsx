6a8df954f79ec841fb0d1843655c8b9ce30b5496
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-08-14 11:59:58 -0700

    i reached newsletterBox part 1.43.55

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LattestCollection from '../components/LattestCollection'
+import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Hero/>
+      <LattestCollection/>
+      <BestSeller/>
+      <OurPolicy/>
+      <NewsLetterBox/>
+    </div>
   )
 }
 
