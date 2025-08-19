bc308e3a1d740378094b740c25b41a4733825841
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-08-19 03:32:26 -0700

    i finished the frontend part

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox.jsx'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={"ABOUT"} text2={"US"}/>
+      </div>
+      <div className='my-10 flex flex-col sm:flex-row gap-16'>
+        <img className='w-full max-w-[450px]' src={assets.about_img} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. ?</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum iste dolorem repudiandae, itaque sit quia illum explicabo ipsum voluptate cum eligendi veritatis, ab unde quo debitis, sunt exercitationem aut ratione?</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore nesciunt omnis blanditiis, ratione, dolore magni esse harum iure debitis quos molestias distinctio, ex fugiat vero fugit doloremque corrupti qui. Sunt!</p>
+        </div>
+      </div>
+      <div className='text-xl py-4 '>
+        <Title text1={"WHY"} text2={"CHOOSE US"}/>
+      </div>
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:mx-16 py-8 sm:py-20 flex-col gap-5'>
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil pariatur rem alias earum perferendis optio! Magnam doloremque eligendi perferendis reiciendis eum libero impedit recusandae minima doloribus? Minus molestias similique nesciunt.</p>
+        </div>
+        <div className='border px-10 md:mx-16 py-8 sm:py-20 flex-col gap-5'>
+          <b>Convenienc:</b>
+          <p className='text-gray-600 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil pariatur rem alias earum perferendis optio! Magnam doloremque eligendi perferendis reiciendis eum libero impedit recusandae minima doloribus? Minus molestias similique nesciunt.</p>
+        </div>
+         <div className='border px-10 md:mx-16 py-8 sm:py-20 flex-col gap-5'>
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil pariatur rem alias earum perferendis optio! Magnam doloremque eligendi perferendis reiciendis eum libero impedit recusandae minima doloribus? Minus molestias similique nesciunt.</p>
+        </div>
+      </div>
+      <NewsLetterBox/>
+    </div>
   )
 }
 
--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,12 +1,12 @@
 
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContect.jsx'
+import { ShopContext } from '../context/ShopContext.jsx'
 import { assets } from '../assets/assets.js'
 import Title from '../components/Title.jsx'
 import ProductItem from '../components/ProductItem.jsx'
 
 const Collection = () => {
-  const {products} = useContext(ShopContext)
+  const {products,search,showSearch} = useContext(ShopContext)
   const [showFilter,setShowFilter] = useState(false)
   const [filterProducts,setFilterProducts]  = useState([])
   const [category,setCategory] = useState([])
@@ -34,6 +34,10 @@ const Collection = () => {
 
   const applyFilter = ()=>{
     let productsCopy = products.slice()
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()))
+    }
     if(category.length > 0){
       productsCopy = productsCopy.filter(item=>category.includes(item.category))
     }
@@ -66,7 +70,7 @@ const Collection = () => {
 
   useEffect(()=>{
     applyFilter()
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct()
